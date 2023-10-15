7b3352a120b526d0f709b6e1aec192e78307fe5b
Alonso-GarnierBBDO <artaviaalonso60@gmail.com>
2023-10-14 21:40:42 -0600

    Se ajusto el sistema

--- src/hook/useLocoScroll.ts
+++ src/hook/useLocoScroll.ts
@@ -37,24 +37,46 @@ const useLocoScroll = () => {
 
                     menssajesElements.forEach( (e, key ) => {
 
-                        if(key == 0 && progress >= 0.20){
-                            e.classList.add('active')
-                        }else if(key == 1 && progress >= 0.25){
-                            e.classList.add('active')
-                        }else if(key == 2 && progress >= 0.30){
-                            e.classList.add('active')
-                        }else if(key == 3 && progress >= 0.35){
-                            e.classList.add('active')
-                        }else if(key == 4 && progress >= 0.40){
-                            e.classList.add('active')
-                        }else if(key == 5 && progress >= 0.45){
-                            e.classList.add('active')
-                        }else if(key == 6 && progress >= 0.50){
-                            e.classList.add('active')
-                        }else if(key == 7 && progress >= 0.55){
-                            e.classList.add('active')
-                        }else if(key >= 8 && progress >= 0.60){
-                            e.classList.add('active')
+                        if(window.innerWidth > 700){
+                            if(key == 0 && progress >= 0.20){
+                                e.classList.add('active')
+                            }else if(key == 1 && progress >= 0.25){
+                                e.classList.add('active')
+                            }else if(key == 2 && progress >= 0.30){
+                                e.classList.add('active')
+                            }else if(key == 3 && progress >= 0.35){
+                                e.classList.add('active')
+                            }else if(key == 4 && progress >= 0.40){
+                                e.classList.add('active')
+                            }else if(key == 5 && progress >= 0.45){
+                                e.classList.add('active')
+                            }else if(key == 6 && progress >= 0.50){
+                                e.classList.add('active')
+                            }else if(key == 7 && progress >= 0.55){
+                                e.classList.add('active')
+                            }else if(key >= 8 && progress >= 0.60){
+                                e.classList.add('active')
+                            }
+                        }else{
+                            if(key == 0 && progress >= 0.10){
+                                e.classList.add('active')
+                            }else if(key == 1 && progress >= 0.15){
+                                e.classList.add('active')
+                            }else if(key == 2 && progress >= 0.20){
+                                e.classList.add('active')
+                            }else if(key == 3 && progress >= 0.25){
+                                e.classList.add('active')
+                            }else if(key == 4 && progress >= 0.30){
+                                e.classList.add('active')
+                            }else if(key == 5 && progress >= 0.35){
+                                e.classList.add('active')
+                            }else if(key == 6 && progress >= 0.40){
+                                e.classList.add('active')
+                            }else if(key == 7 && progress >= 0.45){
+                                e.classList.add('active')
+                            }else if(key >= 8 && progress >= 0.50){
+                                e.classList.add('active')
+                            }
                         }
 
                     })
