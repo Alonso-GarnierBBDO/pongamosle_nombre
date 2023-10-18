9938996da7522c0e1d8277a78b3bf25a4ff9e7a2
Alonso-GarnierBBDO <alonso.artavia@garnierbbdo.com>
2023-10-18 09:41:44 -0600

    Se creo la seccion del esfuerzo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useState } from "react";
 import Image from "next/image";
@@ -17,6 +17,7 @@ import VictimasComponent from "@/components/page/victimas";
 import MotivcacionSegundaComponent from "@/components/page/motivacion_segunda";
 import UneteComponent from "@/components/page/unete";
 import EsfuerzoProyecto from "@/components/page/esfuerso";
+import ImageWord from "@/assets/img/home/word.svg";
 
 type modalContent = {
   active: boolean;
@@ -24,11 +25,10 @@ type modalContent = {
 };
 
 export default function Home() {
-
   useLocoScrollFunction();
 
   const [activeModal, setActiveModal] = useState(false);
-  const [image, setImage] = useState('');
+  const [image, setImage] = useState("");
 
   const handleChildEvent = (eventData: modalContent) => {
     setActiveModal(eventData.active);
@@ -37,70 +37,112 @@ export default function Home() {
 
   const closeModal = () => {
     setActiveModal(false);
-  }
+  };
 
   return (
-      <>
-        <section className='locomotive_scroll'>
-          <section className='sectionScroll'>
-            <InicioComponent/>
-          </section>
-          <section className='sectionScroll mensajes_main'>
-            <MensajesComponents/>
-          </section>
-          <section className="sectionScroll">
-            <ViolenciaComponent/>
-          </section>
-          <section className="sectionScroll">
-            <ComunidadComponent/>
-          </section>
-          <section className="sectionScroll">
-            <PonerleNombreComponent/>
-          </section>
-          <section className="sectionScroll">
-            <MotivcacionComponent/>
-          </section>
-          <section className="sectionScroll">
-            <IniciativaComponent/>
-          </section>
-          <section className="sectionScroll">
-            <CifrasComponent/>
-          </section>
-          <section className="sectionScroll">
-            <VictimasComponent/>
-          </section>
-          <section className="sectionScroll">
-            <MotivcacionSegundaComponent/>
-          </section>
-          <section className="sectionScroll">
-            <UneteComponent/>
-          </section>
-          <section className="sectionScroll">
-            <EsfuerzoProyecto onEvent={handleChildEvent}/>
-          </section>
+    <>
+      <section className="locomotive_scroll">
+        <section className="sectionScroll">
+          <InicioComponent />
         </section>
-        <section className={"modaEsfuerzo" + " " + (activeModal ? 'active' : '')} onClick={closeModal}>
-          <section className="capa">
-            <section className="content" onClick={(e)=>{ e.stopPropagation(); }}>
+        <section className="sectionScroll mensajes_main">
+          <MensajesComponents />
+        </section>
+        <section className="sectionScroll">
+          <ViolenciaComponent />
+        </section>
+        <section className="sectionScroll">
+          <ComunidadComponent />
+        </section>
+        <section className="sectionScroll">
+          <PonerleNombreComponent />
+        </section>
+        <section className="sectionScroll">
+          <MotivcacionComponent />
+        </section>
+        <section className="sectionScroll">
+          <IniciativaComponent />
+        </section>
+        <section className="sectionScroll">
+          <CifrasComponent />
+        </section>
+        <section className="sectionScroll">
+          <VictimasComponent />
+        </section>
+        <section className="sectionScroll">
+          <MotivcacionSegundaComponent />
+        </section>
+        <section className="sectionScroll">
+          <UneteComponent />
+        </section>
+        <section className="sectionScroll">
+          <EsfuerzoProyecto onEvent={handleChildEvent} />
+        </section>
+      </section>
+      <section
+        className={"modaEsfuerzo" + " " + (activeModal ? "active" : "")}
+        onClick={closeModal}
+      >
+        <section className="capa">
+          <section
+            className="content"
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
             <button className="close" onClick={closeModal}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
-                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                fill="currentColor"
+                className="bi bi-x-lg"
+                viewBox="0 0 16 16"
+              >
+                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
               </svg>
             </button>
-              <section className="photo">
-                <Image src={image} width={400} height={400} alt="Photo"/>
-              </section>  
+            <section className="photo">
+              <Image src={image} width={400} height={400} alt="Photo" />
+            </section>
+            <section className="content_item">
+              <section>
+                <h4>¿quiénes son?</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+                </p>
+              </section>
+              <section>
+                <h4>¿cúal es su propósito?</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </section>
+              <section>
+                <h4>¿Por qué apoyan la campaña?</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </section>
+              <section className="actions">
+                <div>
+                  <img src={ImageWord.src} alt="Imagen del enlace" />
+                </div>
+                <a href="@">conoce más</a>
+              </section>
             </section>
           </section>
         </section>
-      </>
-  )
+      </section>
+    </>
+  );
 }
-
 
 function useLocoScrollFunction() {
   useEffect(() => {
     locoScrollFunction();
   }, []);
 }
- 
