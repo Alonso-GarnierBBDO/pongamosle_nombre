0e20ebd43b6560a5c31a922db8122fe2c52ba52e
Alonso-GarnierBBDO <alonso.artavia@garnierbbdo.com>
2023-11-14 10:01:47 -0600

    Se agrego el seo

--- src/components/page/motivacion_segunda.tsx
+++ src/components/page/motivacion_segunda.tsx
@@ -10,19 +10,19 @@ function MotivcacionSegundaComponent(){
 
     const items = [
         {
-            context: 'Luchamos por concientizar acerca de las violencias que sufren las mujeres con discapacidad, incluyendo el acoso, abuso sexual y esterilizaciones forzadas.',
+            context: 'Luchamos por concientizar acerca de las violencias que sufren las mujeres con discapacidad, incluyendo el acoso, abuso sexual y esterilizaciones forzadas',
             title: 'NO ES CUIDARTE, <br/> ES VIOLENTARTE.'
         },
         {
-            context: 'Queremos un mundo donde las mujeres lesbianas, bisexuales y trans puedan vivir su identidad con libertad, expresar afecto sin discriminaciones y que las personas con VIH/Sida tengan acceso a atención preventiva y tratamiento.',
+            context: 'Queremos un mundo donde las mujeres lesbianas, bisexuales y trans puedan vivir su identidad con libertad, expresar afecto sin discriminaciones y que las personas con VIH/Sida tengan acceso a atención preventiva y tratamiento',
             title: 'NO ES SEGUIR LA LEY, <br/> ES VIOLAR TUS DERECHOS HUMANOS.'
         },
         {
-            context: 'Todas las niñas, adolescentes y mujeres jóvenes tienen derecho a una educación sexual completa y al acceso a servicios de salud de alta calidad que prevengan las maternidades forzadas, exámenes de virginidad y matrimonio precoz.',
+            context: 'Todas las niñas, adolescentes y mujeres jóvenes tienen derecho a una educación sexual completa y al acceso a servicios de salud de alta calidad que prevengan las maternidades forzadas, exámenes de virginidad y matrimonio precoz',
             title: 'NO ES PROTECCIÓN, <br/> ES VIOLENCIA.'
         },
         {
-            context: 'Buscamos educar y reconocer las experiencias únicas y desafíos que enfrentan las mujeres migrantes, quienes merecen seguridad, dignidad y atención médica respetuosa en cada etapa de sus vidas.',
+            context: 'Buscamos educar y reconocer las experiencias únicas y desafíos que enfrentan las mujeres migrantes, quienes merecen seguridad, dignidad y atención médica respetuosa en cada etapa de sus vidas',
             title: 'NO ES UNA DECISIÓN MÉDICA, <br/> ES VIOLENCIA OBSTÉTRICA.'
         }
     ]
@@ -52,19 +52,19 @@ function MotivcacionSegundaComponent(){
             <section className={"motivaciones_two scroll" + " " + (removeTree ? 'remove' : '')} ref={elementTree} id="tipos">
                 <section className='escritorio'>
                     <div className="item white">
-                        <span>Luchamos por concientizar acerca de las violencias que sufren las mujeres con discapacidad, incluyendo el acoso, abuso sexual y esterilizaciones forzadas.</span>
+                        <span>Luchamos por concientizar acerca de las violencias que sufren las mujeres con discapacidad, incluyendo el acoso, abuso sexual y esterilizaciones forzadas</span>
                         <strong>NO ES CUIDARTE, ES VIOLENTARTE.</strong>
                     </div>
                     <div className="item dark">
-                        <span>Queremos un mundo donde las mujeres lesbianas, bisexuales y trans puedan vivir su identidad con libertad, expresar afecto sin discriminaciones y que las personas con VIH/Sida tengan acceso a atención preventiva y tratamiento.</span>
+                        <span>Queremos un mundo donde las mujeres lesbianas, bisexuales y trans puedan vivir su identidad con libertad, expresar afecto sin discriminaciones y que las personas con VIH/Sida tengan acceso a atención preventiva y tratamiento</span>
                         <strong>NO ES SEGUIR LA LEY, ES VIOLAR TUS DERECHOS HUMANOS.</strong>
                     </div>
                     <div className="item dark">
-                        <span>Todas las niñas, adolescentes y mujeres jóvenes tienen derecho a una educación sexual completa y al acceso a servicios de salud de alta calidad que prevengan las maternidades forzadas, exámenes de virginidad y matrimonio precoz.</span>
+                        <span>Todas las niñas, adolescentes y mujeres jóvenes tienen derecho a una educación sexual completa y al acceso a servicios de salud de alta calidad que prevengan las maternidades forzadas, exámenes de virginidad y matrimonio precoz</span>
                         <strong>NO ES PROTECCIÓN, ES VIOLENCIA.</strong>
                     </div>
                     <div className="item white">
-                        <span>Buscamos educar y reconocer las experiencias únicas y desafíos que enfrentan las mujeres migrantes, quienes merecen seguridad, dignidad y atención médica respetuosa en cada etapa de sus vidas.</span>
+                        <span>Buscamos educar y reconocer las experiencias únicas y desafíos que enfrentan las mujeres migrantes, quienes merecen seguridad, dignidad y atención médica respetuosa en cada etapa de sus vidas</span>
                         <strong>NO ES UNA DECISIÓN MÉDICA, ES VIOLENCIA OBSTÉTRICA.</strong>
                     </div>
                 </section>
